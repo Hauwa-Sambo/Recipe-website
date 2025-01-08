d650c2549d953b600da7b830280d41e13d41ac19
Hauwa-Sambo <hauwasambo09@gmail.com>
2025-01-08 21:21:15 +0100

    Added html sementics

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-// const specialofferContainer = [
-//     {
-//         specialImages: 'images/special-offer1.png',
-//         specialImages2:'images/rating.svg',
-//         specialImages3:'images/favorite.svg',
-//         address:'Wilderness Club at Big Ceddar',
-//         date:'28 October — 1 November',
-//         price:'$2016',
-//         days:'/6 night'
-//     },
-//     ]
-//     const renderSpecialoffer = specialofferContainer =>{
-//         const Container = document.querySelector('.special-offers');
-//         specialofferContainer.forEach(specialofferContainer => {
-//             Container.innerHTML += `
-//                 <div class="special-img">
-//                     <img src="${specialofferContainer.specialImages}" alt="special-offer1" class="hotel-img">
-//                     <img src="${specialofferContainer.specialImages2}" alt="rating" class="rating">
-//                     <img src="${specialofferContainer.specialImages3}" alt="favorite" class="fav">
-//                 </div>
-//                 <div class="address">
-//                     <h4>${specialofferContainer.address}</h4>
-//                     <p>${specialofferContainer.date}</p>
-//                 </div>
-//                 <div class="price">
-//                     <h5>${specialofferContainer.price}</h5>
-//                     <p class="night">${specialofferContainer.days}</p>
-//                 </div>
-//             `;
-//         }) ;
-//     }
-//     renderSpecialoffer(specialofferContainer);
-    
+// 
+
+/* <div class="meal">
+                // <img src="" alt="" class="meal-image">
+                <h2 class="meal-title"></h2>
+                <p class="instruction"></p>
+                <ul class="ingredients">
+                    <li class="ingredient"></li>
+                    <li class="ingredient"></li>
+                    <li class="ingredient"></li>
+                    <li class="ingredient"></li>
+                    <li class="ingredient"></li>
+                </ul>
+                <button>Watch Video</button>
+            </div> */
